1f5053a66cbc91cd17e466104defd7b81a262342
fred <abnertorres58@gmail.com>
2022-04-12 14:46:38 -0400

    Setting up the environment

--- src/com/freudromero/hibernate/demo/entity/InstructorDetail.java
+++ src/com/freudromero/hibernate/demo/entity/InstructorDetail.java
@@ -1,2 +1,16 @@
-package com.freudromero.hibernate.demo.entity;public class InstructorDetail {
+package com.freudromero.hibernate.demo.entity;
+
+public class InstructorDetail {
+
+    // Annotate the class as an entity and map to a db table
+
+    // Define the fields
+
+    // Annotate the fields with db column names
+
+    // Create constructors
+
+    // Generate getter/setter methods
+
+    // Generate to string method
 }
